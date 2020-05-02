72f52ce57aa8e4a8d8606eaf66a9d64e76e61325
Illarion <hostingerak@mail.ru>
2020-05-03 00:15:43 +0700

    updated

---
+++ EmailSender/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace EmailSender.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        ICommand command = new AsynchronousCommand()
+
+
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
